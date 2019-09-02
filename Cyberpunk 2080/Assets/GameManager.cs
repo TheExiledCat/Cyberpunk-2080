2ee107515eab1abf0d87bff317286a00088343b1
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-09-02 14:14:33 +0200

    asd

---
+++ Cyberpunk 2080/Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int frames=0;
+    public GameObject building;
+    public static GameManager GM;
+    public float BPM;
+    public float turnSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BPM = 80;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frames++;
+        if (frames >= 59)
+        {
+            SpawnBuilding();
+            frames = 0;
+        }
+        transform.Rotate(0, 0,-Input.GetAxis("Horizontal")* turnSpeed);
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    void SpawnBuilding()
+    {
+
+    }
+}
---
+++ Cyberpunk 2080/Assets/Player_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    
+    public float jHeight;
+     float moveIn;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
