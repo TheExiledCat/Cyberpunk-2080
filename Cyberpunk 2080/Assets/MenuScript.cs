c4204177ee8fb8686f560d7a100784825c8687b2
Jaimy Koster <43574560+RekCus@users.noreply.github.com>
2019-09-03 08:50:24 +0200

    Tv problem fixed

--- Cyberpunk 2080/Assets/MenuScript.cs
+++ Cyberpunk 2080/Assets/MenuScript.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string sceneToLoad;
 
-    // Update is called once per frame
-    void Update()
+    public void loadNextScene()
     {
-        
+        SceneManager.LoadScene(sceneToLoad);
+    }
+    
+    public void exitGame()
+    {
+        Application.Quit();
     }
 }
+
